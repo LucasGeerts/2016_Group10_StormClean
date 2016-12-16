a3d2e2cfdcc07bfb9a3cf4d77baa91e118cdf6c7
DKersbergen <d.kersbergen@outlook.com>
2016-12-16 13:28:07 +0100

    New changes
    stacked widget working
    buttons assigned

--- StormClean/StormClean_dialog.py
+++ StormClean/StormClean_dialog.py
@@ -21,10 +21,11 @@
  ***************************************************************************/
 """
 
+
 import os
 from PyQt4 import QtGui, uic
 from qgis.core import *
-from PyQt4.QtCore import QFileInfo
+from PyQt4 import QtCore
 from . import utility_functions as uf
 import os.path
 from qgis.gui import *
@@ -43,13 +44,28 @@ class StormCleanDialog(QtGui.QDialog, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+
     # define globals
         self.iface = iface
         self.canvas = self.QgsMapCanvas
 
+    #GUI
+        self.Pages.setCurrentIndex(0)
+    #Interaction
+        self.Login.clicked.connect(self.buttonOKclicked)
+        self.Cancel.clicked.connect(self.close)
+        self.pushButton_logout.clicked.connect(self.Logoutclicked)
+        self.pushButton_logout2.clicked.connect(self.Logoutclicked)
+        self.pushButton_logout3.clicked.connect(self.Logoutclicked)
+        self.pushButton_logout4.clicked.connect(self.Logoutclicked)
+        self.pushButton_logout5.clicked.connect(self.Logoutclicked)
+
     def showEvent(self, event):
+        self.Pages.setCurrentIndex(0)
+        self.Box_Password.clear()
+        self.Box_Username.clear()
         scenario_open = False
-        scenario_file = os.path.join('C:\Users\Python\Desktop\Plugin\StormClean','SampleData','Rotterdam.qgs')
+        scenario_file = os.path.join(os.path.dirname(__file__),'SampleData','Rotterdam.qgs')
         # check if file exists
         if os.path.isfile(scenario_file):
             self.iface.addProject(scenario_file)
@@ -60,6 +76,64 @@ class StormCleanDialog(QtGui.QDialog, FORM_CLASS):
             if new_file:
                 self.iface.addProject(unicode(new_file))
                 scenario_open = True
+
+    def buttonOKclicked(self):
+        self.Pages.setCurrentIndex(1)
+        layer = uf.getLegendLayerByName(self.iface, 'CBS_2015_Rotterdam_buurt_Edit')
+        if not layer.isValid():
+            raise IOError, "Failed to open the layer"
+        QgsMapLayerRegistry.instance().addMapLayer(layer)
+        self.canvas.setExtent(layer.extent())
+        self.canvas.setLayerSet([QgsMapCanvasLayer(layer)])
+
+    def Logoutclicked(self):
+        msgBox = QtGui.QMessageBox()
+        msgBox.setText('Are you sure you want to log out?')
+        msgBox.addButton(QtGui.QPushButton('No'), QtGui.QMessageBox.RejectRole)
+        msgBox.addButton(QtGui.QPushButton('Yes'), QtGui.QMessageBox.AcceptRole)
+        ret = msgBox.exec_()
+        print ret
+        if ret == 0:
+            pass
+        elif ret == 1:
+            self.Pages.setCurrentIndex(0)
+            self.Box_Password.clear()
+            self.Box_Username.clear()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 
         for layer in uf.getCanvasLayers(self.iface, geom='all', provider='all'):
         #layer = uf.getLegendLayerByName(self.iface, '2016_Rotterdam_trees_Edit')
@@ -75,3 +149,4 @@ class StormCleanDialog(QtGui.QDialog, FORM_CLASS):
 
         # set the map canvas layer set
             self.canvas.setLayerSet([QgsMapCanvasLayer(layer)])
+"""
