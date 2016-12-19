47f73807ca29a64c5e8e680d0cae15d3cd7b17ac
DKersbergen <d.kersbergen@outlook.com>
2016-12-19 17:51:40 +0100

    Edits on new pages
    Map implementation works,
    Zooming to selection, multiple layers
    Multiple map spaces and multiple pages

--- StormClean/StormClean_dialog.py
+++ StormClean/StormClean_dialog.py
@@ -48,6 +48,9 @@ class StormCleanDialog(QtGui.QDialog, FORM_CLASS):
     # define globals
         self.iface = iface
         self.canvas = self.QgsMapCanvas
+        self.canvas_1 = self.QgsMapCanvas_1
+        self.canvas_2 = self.QgsMapCanvas_2
+
 
     #GUI
         self.Pages.setCurrentIndex(0)
@@ -59,6 +62,7 @@ class StormCleanDialog(QtGui.QDialog, FORM_CLASS):
         self.pushButton_logout3.clicked.connect(self.Logoutclicked)
         self.pushButton_logout4.clicked.connect(self.Logoutclicked)
         self.pushButton_logout5.clicked.connect(self.Logoutclicked)
+        self.listWidget_listofneighbourhood.currentItemChanged.connect(self.returnitem)
 
     def showEvent(self, event):
         self.Pages.setCurrentIndex(0)
@@ -79,26 +83,64 @@ class StormCleanDialog(QtGui.QDialog, FORM_CLASS):
 
     def buttonOKclicked(self):
         self.Pages.setCurrentIndex(1)
-        layer = uf.getLegendLayerByName(self.iface, 'CBS_2015_Rotterdam_buurt_Edit')
-        if not layer.isValid():
-            raise IOError, "Failed to open the layer"
-        QgsMapLayerRegistry.instance().addMapLayer(layer)
-        self.canvas.setExtent(layer.extent())
-        self.canvas.setLayerSet([QgsMapCanvasLayer(layer)])
+        if self.canvas.layerCount() == 0:
+            #Open map to canvas
+            canvas_layers = []
+            layer1 = uf.getLegendLayerByName(self.iface, 'Wijk_Rott')
+            layer2 = uf.getLegendLayerByName(self.iface, 'BackgroundGrey')
+            layers = [layer1, layer2]
+            for layer in layers:
+                if not layer.isValid():
+                    raise IOError, "Failed to open the layer"
+                QgsMapLayerRegistry.instance().addMapLayer(layer)
+                canvas_layers.append(QgsMapCanvasLayer(layer))
+            self.canvas.setExtent(layer1.extent())
+            self.canvas.setLayerSet(canvas_layers)
+
+            #Fill attributes in list
+            self.listWidget_listofneighbourhood.clear()
+            for item in uf.getFieldValues(layer1, 'WK_NAAM')[0]:
+                self.listWidget_listofneighbourhood.addItem(item)
+
 
     def Logoutclicked(self):
         msgBox = QtGui.QMessageBox()
+        msgBox.setWindowTitle('StormClean')
         msgBox.setText('Are you sure you want to log out?')
         msgBox.addButton(QtGui.QPushButton('No'), QtGui.QMessageBox.RejectRole)
         msgBox.addButton(QtGui.QPushButton('Yes'), QtGui.QMessageBox.AcceptRole)
         ret = msgBox.exec_()
-        print ret
         if ret == 0:
             pass
         elif ret == 1:
             self.Pages.setCurrentIndex(0)
             self.Box_Password.clear()
             self.Box_Username.clear()
+
+    def returnitem(self, name=''):
+        layer = uf.getLegendLayerByName(self.iface, 'Wijk_Rott')
+        expr = QgsExpression('"WK_NAAM"  =  ' + "'" + str(name.text()) + "'")
+        attr = layer.getFeatures(QgsFeatureRequest(expr))
+        ids = [att.id() for att in attr]
+        layer.setSelectedFeatures(ids)
+        self.canvas.zoomToSelected(layer)
+
+        msgBox = QtGui.QMessageBox()
+        msgBox.setWindowTitle('StormClean')
+        msgBox.setText('Would you like to select'+ str(name.text() + '?'))
+        msgBox.addButton(QtGui.QPushButton('No'), QtGui.QMessageBox.RejectRole)
+        msgBox.addButton(QtGui.QPushButton('Yes'), QtGui.QMessageBox.AcceptRole)
+        ret = msgBox.exec_()
+        if ret == 0:
+            pass
+        elif ret == 1:
+            self.Pages.setCurrentIndex(2)
+            self.loadpage3()
+        layer.removeSelection()
+        self.canvas.setExtent(layer.extent())
+        self.canvas.refresh()
+
+
 
 
 
